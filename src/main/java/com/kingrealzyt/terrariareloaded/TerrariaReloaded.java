94c1614f90e58f0b2679f5a92f5041f9bae4a659
KingRealzYT <41351144+KingRealzYT@users.noreply.github.com>
2020-09-13 21:42:16 -0500

    First Commit of all Files

---
+++ src/main/java/com/kingrealzyt/terrariareloaded/TerrariaReloaded.java
@@ -0,0 +1,48 @@
+package com.kingrealzyt.terrariareloaded;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("terrariareloaded")
+public class TerrariaReloaded
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public TerrariaReloaded() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
+
+    }
+}
