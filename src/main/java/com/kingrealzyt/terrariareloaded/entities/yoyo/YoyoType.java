79e9dd68e383fffe14dbdbfaa7861fbf4f17bac8
_Cookie_ <weidlichnathan@gmail.com>
2020-09-22 16:40:46 -0700

    Better Balanced Yoyos

--- src/main/java/com/kingrealzyt/terrariareloaded/entities/yoyo/YoyoType.java
+++ src/main/java/com/kingrealzyt/terrariareloaded/entities/yoyo/YoyoType.java
@@ -6,28 +6,28 @@ import net.minecraft.util.ResourceLocation;
 public enum YoyoType {
 
     //=========PRE_HARDMODE=========
-    AMAZON(18, 3.75F, 6.5, 160, TerrariaReloaded.rlTexture("items/yoyo_amazon.png")),
-    ARTERY(17, 4, 6.25, 120, TerrariaReloaded.rlTexture("items/yoyo_artery.png")),
-    MALAISE(16, 4.5F, 7, 140, TerrariaReloaded.rlTexture("items/yoyo_malaise.png")),
-    RALLY(14.0F, 3.5F, 5, 100, TerrariaReloaded.rlTexture("items/yoyo_rally.png")),
-    WOODEN(9, 2.5F, 4.5F, 80, TerrariaReloaded.rlTexture("items/yoyo_wooden.png")),
-    CODE_1(21, 3.25F, 6.75, 180, TerrariaReloaded.rlTexture("items/yoyo_code_1.png")),
-    VALOR(22, 3.85F, 7, 220, TerrariaReloaded.rlTexture("items/yoyo_valor.png")),
-    CASCADE(27, 4.3F, 7.75, 320, TerrariaReloaded.rlTexture("items/yoyo_cascade.png")),
+    AMAZON(9, 3.75F, 6.5, 160, TerrariaReloaded.rlTexture("items/yoyo_amazon.png")),
+    ARTERY(8, 4, 6.25, 120, TerrariaReloaded.rlTexture("items/yoyo_artery.png")),
+    MALAISE(7, 4.5F, 7, 140, TerrariaReloaded.rlTexture("items/yoyo_malaise.png")),
+    RALLY(6.0F, 3.5F, 5, 100, TerrariaReloaded.rlTexture("items/yoyo_rally.png")),
+    WOODEN(4, 2.5F, 4.5F, 80, TerrariaReloaded.rlTexture("items/yoyo_wooden.png")),
+    CODE_1(10, 3.25F, 6.75, 180, TerrariaReloaded.rlTexture("items/yoyo_code_1.png")),
+    VALOR(10, 3.85F, 7, 220, TerrariaReloaded.rlTexture("items/yoyo_valor.png")),
+    CASCADE(12, 4.3F, 7.75, 320, TerrariaReloaded.rlTexture("items/yoyo_cascade.png")),
 
     //=========HARDMODE=========
-    FORMAT_C(35, 3.25F, 8, 160, TerrariaReloaded.rlTexture("items/yoyo_format_c.png")),
-    CHICK(30, 3.3F, 8.5, 320, TerrariaReloaded.rlTexture("items/yoyo_chick.png")),
-    GRADIENT(44, 3.8F, 7.75, 200, TerrariaReloaded.rlTexture("items/yoyo_gradient.png")),
-    HEL_FIRE(45, 4.5F, 8.5, 210, TerrariaReloaded.rlTexture("items/yoyo_hel_fire.png")),
-    AMAROK(47, 2.8F, 8.25, 300, TerrariaReloaded.rlTexture("items/yoyo_amarok.png")),
-    CODE_2(54, 3.8F, 9, 1200, TerrariaReloaded.rlTexture("items/yoyo_code_2.png")),
-    YELETS(40, 3.1F, 15, 280, TerrariaReloaded.rlTexture("items/yoyo_yelets.png")),
-    REDS_THROW(70, 4.5F, 11.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_reds_throw.png")),
-    VALKYRIE_YOYO(70, 4.5F, 11.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_valkyrie_yoyo.png")),
-    KRAKEN(95, 4.3F, 10.5D, 1200, TerrariaReloaded.rlTexture("items/yoyo_kraken.png")),
-    THE_EYE_OF_CTHULHU(115, 3.5F, 11.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_the_eye_of_cthulu.png")),
-    TERRARIAN(190, 6.5F, 22.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_terrarian.png"));
+    FORMAT_C(16, 3.25F, 8, 160, TerrariaReloaded.rlTexture("items/yoyo_format_c.png")),
+    CHICK(17, 3.3F, 8.5, 320, TerrariaReloaded.rlTexture("items/yoyo_chick.png")),
+    GRADIENT(20, 3.8F, 7.75, 200, TerrariaReloaded.rlTexture("items/yoyo_gradient.png")),
+    HEL_FIRE(21, 4.5F, 8.5, 210, TerrariaReloaded.rlTexture("items/yoyo_hel_fire.png")),
+    AMAROK(21, 2.8F, 8.25, 300, TerrariaReloaded.rlTexture("items/yoyo_amarok.png")),
+    CODE_2(24, 3.8F, 9, 1200, TerrariaReloaded.rlTexture("items/yoyo_code_2.png")),
+    YELETS(18, 3.1F, 15, 280, TerrariaReloaded.rlTexture("items/yoyo_yelets.png")),
+    REDS_THROW(32, 4.5F, 11.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_reds_throw.png")),
+    VALKYRIE_YOYO(32, 4.5F, 11.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_valkyrie_yoyo.png")),
+    KRAKEN(43, 4.3F, 10.5D, 1200, TerrariaReloaded.rlTexture("items/yoyo_kraken.png")),
+    THE_EYE_OF_CTHULHU(52, 3.5F, 11.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_the_eye_of_cthulu.png")),
+    TERRARIAN(86, 6.5F, 22.5, 1200, TerrariaReloaded.rlTexture("items/yoyo_terrarian.png"));
 
     public static final YoyoType[] VALUES = values();
 
